eaff787a6bbb2b1227f4a9f157966f8c2f1f3ed3
magesh <hariarunengg@gmail.com>
2022-03-31 19:38:11 +0530

    push to nft page btn added

--- components/Home/Banner.jsx
+++ components/Home/Banner.jsx
@@ -1,9 +1,15 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 const style = {}
 const Banner = () => {
+  const router = useRouter()
   return (
     <div className="h-[500px] w-[900px] bg-white">
       <img
+        className="cursor-pointer"
+        onClick={() => {
+          router.push('nft/magesh')
+        }}
         src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg"
         alt=""
       />
